091d7674c8a060b84e76b26e47ebc2fc54b295e0
Aeris <aeris@imirhil.fr>
2016-01-18 00:01:08 +0100

    More press articles

--- press.rb
+++ press.rb
@@ -1,39 +1,58 @@
 #!/usr/bin/env ruby
 require 'open-uri'
 require 'csv'
-csv = CSV.new open 'https://framacalc.org/ssa_press.csv'
+require 'awesome_print'
+csv     = CSV.new open 'https://framacalc.org/ssa_press.csv'
 content = {}
-lang = nil
+lang    = nil
 csv.each do |row|
 	if not row[0].nil? and row[1].nil?
-		lang = row[0].downcase
-		content[lang] = [] unless content.include? lang
+		lang          = row[0].downcase
+		content[lang] = {} unless content.include? lang
 	elsif not row[1].nil?
-		date = row.first
-		date = DateTime.parse date if date
-		row[0] = date
-		content[lang] << row
+		date               = DateTime.parse row.first
+		row[0]             = date
+		day                = date.to_date
+		content[lang][day] = [] unless content[lang].include? day
+		content[lang][day] << row
 	end
 end
-content = content.map do |lang, articles|
-	articles.sort! do |a, b|
-		a, b = a.first, b.first
-		next 1 unless a
-		next -1 unless b
-		a <=> b
-	end
-	[lang, articles]
-end.to_h
+# content = content.map do |lang, date|
+# 	articles.sort! do |a, b|
+# 		a, b = a.first, b.first
+# 		next 1 unless a
+# 		next -1 unless b
+# 		b <=> a
+# 	end
+# 	[lang, articles]
+# end.to_h
+
 lines = []
-content.each do |lang, articles|
-	lines << "= image_tag '#{lang}_big.png', alt: '#{lang.upcase}'"
-	lines << '%ul'
-	articles.each do |date, url, media, title|
+content.each do |lang, days|
+	lines << [
+			"= image_tag '#{lang}_big.png', alt: '#{lang.upcase}'",
+			'%ul'
+	]
+
+	days.keys.sort.reverse.each do |day|
 		lines << [
-			"\t%li",
-			"\t\t#{date ? "(#{date.strftime '%d/%m/%Y'}) " : ''}#{media}",
-			"\t\t%a{href: '#{url}'} #{title}"
+				"\t%li",
+				"\t\t#{day.strftime '%d/%m/%Y'}",
+				"\t\t%ul"
 		]
+
+		days[day].sort do |a, b|
+			a, b = a.first, b.first
+			next 1 unless a
+			next -1 unless b
+			b <=> a
+		end.each do |_, url, media, title|
+			lines << [
+					"\t\t\t%li",
+					"\t\t\t\t#{media}",
+					"\t\t\t\t%a{href: '#{url}'} #{title}"
+			]
+		end
 	end
 end
 puts lines.join "\n"
