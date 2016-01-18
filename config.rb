2f8cf21d447e9d4c12c73443abf3115cbe3aa968
Aeris <aeris@imirhil.fr>
2016-01-18 22:54:47 +0100

    Some fix before public release

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :haml, format: :html5
+activate :sprockets
 
 configure :development do
 	activate :livereload, apply_css_live: true, apply_js_live: true, no_swf: true, host: 'localhost'
@@ -9,6 +11,4 @@ end
 configure :build do
 	activate :minify_css
 	activate :minify_javascript
-
-	set :http_prefix, '/ssa'
 end
